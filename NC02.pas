39c918c296f12f9db42d31e49c960b07ef8fabea
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-04 02:45:50 +0100

    Update NC02.pas

--- NC02.pas
+++ NC02.pas
@@ -1,20 +1,17 @@
-//NC02(regrmeth 1...,x,y)/D.G.SCHRAUSSER/2025
+//NC02(reg.meth[1..],x,y)/D.G.SCHRAUSSER/2025
 //e.g.NC02(1,"TN7","VL7")
 #cas
 NC02(r,X,Y):=
 BEGIN
-//x
-X▶S1(1);
-//y
-Y▶S1(2);
+//x,y,reg.meth
+X▶S1(1);Y▶S1(2);
 r▶S1(4);
 Do2VStats(S1);
 STARTAPP("Statistics_2_Var");
 STARTVIEW(1);
-//r, r², det%
-Corr▶L1(1);
-CoefDet▶L2(1);
-CoefDet*100▶L3(1);
+//r,r²,reg.coef
+Corr▶L1(1); CoefDet▶L2(1);
+S1(5)▶L3(1);
 L1(1),L2(1),L3(1);
 END;
 #end
